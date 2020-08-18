0531d9c83c953305a50f31311dbc543f0efc8dce
Shelkov Dmitriy <bobyorrr@gmail.com>
2020-08-18 14:18:36 +0300

    Скрипт для бургер меню

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.scss";
-console.log("hello world!");
+
+// Скрипт для бургер меню
+const burgerButton = document.querySelectorAll(".main-manu__burger-button");
+const burgerManu = document.querySelector(".burger-manu");
+
+for (const i of burgerButton) {
+	i.addEventListener("click", () => {
+		burgerManu.classList.toggle("burger-manu__is-active");
+		for (const b of burgerButton) {
+			b.classList.toggle("main-manu__burger-button-cross");
+		}
+	});
+}
